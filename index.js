9f05f9ff10e15ba2bcc50f7813e674112a62e3fe
= <ibm@ibms-MacBook-Pro.local>
2018-04-28 23:07:32 -0500

    second code upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express'); //Require express, the lightweight server module
 const bodyParser = require("body-parser"); //Require bodyparser, a tool necessary to parse POST values
-const formidable = require('formidable');
-const fs = require('fs');
+const fs = require('fs-extra');
 //Initialize the app values
 const app = express();
 //Allow the app to parse values
@@ -11,7 +10,17 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 //This allows our server to host files
-app.use(express.static(__dirname + '/img'));
+//app.use(express.static(__dirname + '/img'));
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+  //__dirname is the directory in nodeJS, useful for building paths
+  res.sendFile(__dirname + '/public/ui/login.html');
+  }
+);
+
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
